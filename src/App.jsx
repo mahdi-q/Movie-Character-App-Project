aab9c1645d3566e94bdbce37be9859027369a03e
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2024-06-10 16:15:17 +0330

    developing CharacterList & Character component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import "./App.css";
+import { allCharacters } from "../data/data";
+import CharacterList from "./components/CharacterList";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div className="app">
+      <Navbar />
+
+      <div className="main">
+        <CharacterList characters={allCharacters} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
